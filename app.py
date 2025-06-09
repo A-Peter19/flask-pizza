86d20cc086b7e2fc2419aee137b4a48232e5696e
Peter <pa.alborzpour@hotmail.com>
2025-06-09 17:30:22 +0100

    Initial commit of all project files

---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    # Looks for template
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/user/<name>')
+def user(name):
+    # passes the name variable into the user.html template.
+    return render_template('user.html', name=name)
+
+@app.route('/form')
+def form():
+    return render_template('form.html')
+
+@app.route('/posts')
+def posts():
+    sample_posts = [
+        {'title': 'Hello Flask!', 'body': 'This is my first post.'},
+        {'title': 'Another update', 'body': 'More content here.'}
+    ]
+    # This renders a template that shows your posts.
+    return render_template('posts.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    # this grabs the data from the form : the username inputted.
+    username = request.form.get('username')
+    if not username:
+        # If no name is provided, redirect the user back to the form.
+        return redirect(url_for('form'))
+    # otherwise show thank you page.
+    return render_template('thankyou.html', name=username)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
